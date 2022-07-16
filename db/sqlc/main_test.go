fa8c32f88c80201042b7f177a7c8bc47ae8bf982
tanvirhasan74 <tshuvo74@gmail.com>
2022-07-17 04:10:35 +0900

    change main test file for CI action check

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,19 +7,19 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-	"github.com/tanvirhasan74/transaction_system/util"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	config, err := util.LoadConfig("../..")
-	if err != nil {
-		log.Fatal("cannot load config:", err)
-	}
-
-	testDB, err = sql.Open(config.DBDriver, config.DBSource)
+	var err error
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
