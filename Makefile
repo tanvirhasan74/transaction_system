7da93a017fbe2f44bcb4fd154127bee40a7bb8ed
tanvirhasan74 <tshuvo74@gmail.com>
2022-07-17 12:32:39 +0900

    initialize gin framework and create account api

--- Makefile
+++ Makefile
@@ -20,3 +20,8 @@ sqlc:
 
 test:
 	go test -v -cover ./...
+
+server:
+	go run main.go
+
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc test server
--- db/sqlc/account_test.go
+++ db/sqlc/account_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/require"
-	"github.com/techschool/simplebank/util"
+	"github.com/tanvirhasan74/transaction_system/util"
 )
 
 func createRandomAccount(t *testing.T) Account {
